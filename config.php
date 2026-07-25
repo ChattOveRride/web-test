2d6302cc5dbd0dab7c4aa410e940d99062524b1e
Yahueh29 <yashielmelgarejo5@gmail.com>
2026-07-24 20:25:31 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -10,5 +10,7 @@ $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: ". $conn->connect_error);
 }
-
+        define('GOOGLE_CLIENT_ID', '');
+        define('GOOGLE_CLIENT_SECRET', '');
+        define('GOOGLE_REDIRECT_URI', 'http://localhost/WEB-TEST-MAIN/google-callback.php');
 ?>
